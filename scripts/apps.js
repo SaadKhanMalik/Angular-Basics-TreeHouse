6d939517b1cc81c2b7484e03d09df70deeb06a0c
SKM-MAC <social@saadkhanmalik.com>
2016-09-02 17:20:26 +0300

    General course updates

--- scripts/apps.js
+++ scripts/apps.js
@@ -3,4 +3,18 @@ angular.module("todoListApp", []);
   $scope.helloWorld = function(){
     console.log("loging some text!!");
   };
+
+
+  $scope.todos = [
+    {"name": "clean the house"},
+    {"name": "wash the dog"},
+    {"name": "water the lawn"},
+    {"name": "pay the bills"},
+    {"name": "run"},
+    {"name": "swim"}
+  ]
+
+
+
+
 });
