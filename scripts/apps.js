259b530e53e6796c884de17fbfad2509227a37cb
SKM-MAC <social@saadkhanmalik.com>
2016-09-04 23:49:24 +0300

    Stage Finished

--- scripts/apps.js
+++ scripts/apps.js
@@ -1,20 +1,20 @@
-angular.module("todoListApp", []);
-.controller('mainCtrl', function($scope){
-  $scope.helloWorld = function(){
-    console.log("loging some text!!");
-  };
+angular.module("todoListApp", [])
 
+.controller('mainCtrl', function($scope) {
+  $scope.helloWorld = function() {
+    console.log("Hello there! This the helloWorld controller function, in the mainCtrl!");
+  };
+    $scope.learningNgChange = function() {
+    console.log("Input changed!");
+  };
 
   $scope.todos = [
     {"name": "clean the house"},
-    {"name": "wash the dog"},
+    {"name": "feed the dog"},
     {"name": "water the lawn"},
     {"name": "pay the bills"},
     {"name": "run"},
     {"name": "swim"}
   ]
-
-
-
 
 });
